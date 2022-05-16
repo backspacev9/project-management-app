3c88489df04cecc2f5f8473022f87bda8dabb12a
backspacev9 <backspacev9@gmail.com>
2022-05-16 03:31:10 +0300

    refactor: add rout to board

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header';
+import BoardPage from './pages/board-page';
 import ErrorPage from './pages/error-page';
 import MainPage from './pages/main-page';
 
@@ -11,6 +12,7 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/b/:id" element={<BoardPage />} />
           <Route path="/404" element={<ErrorPage />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
