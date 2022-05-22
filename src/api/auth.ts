44e09db6f43f80e3b8964885a1047ab5e0d16f17
anaya-che <anaya.che22@gmail.com>
2022-05-22 22:41:27 +0300

    refactor: api request

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,24 +3,24 @@ import { IToken, IUserInfo } from '../utils/auth-types';
 import { BASE_URL } from './consts';
 
 export const signUp = async (
-  userName: string,
-  userLogin: string,
-  userPassword: string
+  name: string,
+  login: string,
+  password: string
 ): Promise<void | IUserInfo> => {
   return axios
     .post(`${BASE_URL}signup`, {
-      name: userName,
-      login: userLogin,
-      password: userPassword,
+      name,
+      login,
+      password,
     })
     .then((res): Promise<IUserInfo> => res.data);
 };
 
-export const signIn = async (userLogin: string, userPassword: string): Promise<void | IToken> => {
+export const signIn = async (login: string, password: string): Promise<void | IToken> => {
   return axios
     .post(`${BASE_URL}signin`, {
-      login: userLogin,
-      password: userPassword,
+      login,
+      password,
     })
     .then((res): Promise<IToken> => res.data);
 };
