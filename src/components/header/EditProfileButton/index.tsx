8e541edaef7a4caaf2b9229c99fcd1c98dde2c59
anaya-che <anaya.che22@gmail.com>
2022-05-25 00:42:29 +0300

    refactor: delete unused code

--- src/components/header/EditProfileButton/index.tsx
+++ src/components/header/EditProfileButton/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
 import { RootState } from '../../../redux/store';
 import { getCurrentUser } from '../../../redux/users-reducer';
@@ -7,12 +7,9 @@ import { getCurrentUser } from '../../../redux/users-reducer';
 const EditProfileButton = () => {
   const dispatch = useAppDispatch();
   const { token, userId } = useAppSelector((state: RootState) => state.auth);
-  const navigation = useNavigate();
 
   const handleEditProfile = () => {
-    dispatch(getCurrentUser({ token, id: userId })).then(() =>
-      navigation('/edit-profile', { replace: true })
-    );
+    dispatch(getCurrentUser({ token, id: userId }));
   };
 
   return (
