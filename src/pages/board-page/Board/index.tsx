d8a871c41f771a47a62a51c863ad028b4f12c715
anaya-che <anaya.che22@gmail.com>
2022-05-29 02:41:08 +0300

    refactor: board page

--- src/pages/board-page/Board/index.tsx
+++ src/pages/board-page/Board/index.tsx
@@ -2,8 +2,7 @@ import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
 import Column from '../Column';
 import { RootState } from '../../../redux/store';
 import { useEffect } from 'react';
-import { createOneColumn, updateOneColumn } from '../../../redux/columns-reducer';
-import BoardHeader from '../components/header';
+import { updateOneColumn } from '../../../redux/columns-reducer';
 import { useParams } from 'react-router-dom';
 import { getBoardByID, setColumns, setTasks } from '../../../redux/boards-reducer';
 import BtnAddColumn from '../../../components/board/btn-addColumn';
@@ -14,9 +13,7 @@ import { ITaskWithFiles } from '../../../utils/task-types';
 import { updateOneTask } from '../../../redux/tasks-reducer';
 import './index.scss';
 import Preloader from '../../../components/preloader';
-import EditProfileButton from '../../../components/header/EditProfileButton';
-import { LocaleSelect } from '../../../components/header/LocalesSelect';
-import SignOutButton from '../../../components/header/SignOut';
+import Header from '../../../components/header';
 
 const Board = () => {
   const { token } = useAppSelector((state: RootState) => state.auth);
@@ -90,7 +87,6 @@ const Board = () => {
         })
       );
       setBoard();
-      console.log(orderTask);
     }
 
     if (type === DropColumnType) {
@@ -118,20 +114,14 @@ const Board = () => {
 
   return (
     <>
-      <header className="header">
-        <EditProfileButton />
-        <SignOutButton />
-        <LocaleSelect />
-      </header>
+      <Header />
       {isFetch ? (
         <Preloader />
       ) : (
         <div className="Board">
-          <BoardHeader title={currentBoard.title} />
-
           <DragDropContext onDragEnd={(param) => dragEnd(param)}>
             <Droppable droppableId="board-drop-area" type={DropColumnType} direction="horizontal">
-              {(provided, snapshot) => (
+              {(provided) => (
                 <div
                   className="board-columns-container"
                   ref={provided.innerRef}
@@ -140,7 +130,7 @@ const Board = () => {
                   {columns && Object.keys(columns).length !== 0
                     ? columns.map((el, index) => (
                         <Draggable key={index} draggableId={`cdrag-${index}`} index={index}>
-                          {(providedDrag, snapshot) => (
+                          {(providedDrag) => (
                             <Droppable droppableId={el.id} type={DropTaskType}>
                               {(providedDrop, snapshotDrop) => (
                                 <Column
