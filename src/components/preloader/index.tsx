8f4de762c1fc60511f00cde99453e88918678a38
m.filionchyk <maria.filenchik@yandex.by>
2022-05-29 00:45:49 +0300

    fix: change preloader style

--- src/components/preloader/index.tsx
+++ src/components/preloader/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './index.scss';
 
 const Preloader = () => {
-  return <div className="preloader"></div>;
+  return (
+    <div className="preloader-container">
+      <div className="preloader"></div>
+    </div>
+  );
 };
 
 export default Preloader;
