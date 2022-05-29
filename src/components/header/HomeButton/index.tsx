4b1eae2aee0f30dd1549b7b9065a9d886d3534ce
anaya-che <anaya.che22@gmail.com>
2022-05-30 00:16:54 +0300

    feat: add style for registration and profile page

--- src/components/header/HomeButton/index.tsx
+++ src/components/header/HomeButton/index.tsx
@@ -4,7 +4,13 @@ import { NavLink, useLocation } from 'react-router-dom';
 const HomeButton = () => {
   const location = useLocation();
   return (
-    <NavLink to={location.pathname.includes('/main/b/') ? '/main' : '/'}>
+    <NavLink
+      to={
+        location.pathname.includes('/main/b/') || location.pathname.includes('/edit-profile')
+          ? '/main'
+          : '/'
+      }
+    >
       <button className="home-btn"></button>
     </NavLink>
   );
--- src/pages/registration/index.tsx
+++ src/pages/registration/index.tsx
@@ -23,41 +23,43 @@ const Registration = () => {
   return (
     <>
       <section className="registration">
-        <h3>{t('registration')}</h3>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            type="text"
-            placeholder={t('name')}
-            id="user-name"
-            autoComplete="off"
-            {...register('name', { required: true, pattern: /^[A-Za-zА-Яа-яЁё]+$/ })}
-          />
-          <div className="error-message">{errors.name && t('name_error')}</div>
-          <input
-            type="text"
-            placeholder={t('login')}
-            id="user-login"
-            autoComplete="off"
-            {...register('login', { required: true, pattern: /^[A-Za-zА-Яа-яЁё0-9]+$/ })}
-          />
-          <div className="error-message">{errors.login && t('login_error')}</div>
-          <input
-            type="password"
-            placeholder={t('password')}
-            id="user-password"
-            autoComplete="off"
-            {...register('password', { required: true, pattern: /^[A-Za-zА-Яа-яЁё0-9]+$/ })}
-          />
-          <div className="error-message">{errors.password && t('password_error')}</div>
-          <button type="submit" className="registration-btn">
-            {t('sign_Up')}
-          </button>
-        </form>
-        <div>
-          <span>{t('registration_msg')}</span>
-          <NavLink to="/signin">
-            <span className="page-link">{t('to_autorization_page')}</span>
-          </NavLink>
+        <div className="registration-container">
+          <h3>{t('registration')}</h3>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              placeholder={t('name')}
+              id="user-name"
+              autoComplete="off"
+              {...register('name', { required: true, pattern: /^[A-Za-zА-Яа-яЁё]+$/ })}
+            />
+            <div className="error-message">{errors.name && t('name_error')}</div>
+            <input
+              type="text"
+              placeholder={t('login')}
+              id="user-login"
+              autoComplete="off"
+              {...register('login', { required: true, pattern: /^[A-Za-zА-Яа-яЁё0-9]+$/ })}
+            />
+            <div className="error-message">{errors.login && t('login_error')}</div>
+            <input
+              type="password"
+              placeholder={t('password')}
+              id="user-password"
+              autoComplete="off"
+              {...register('password', { required: true, pattern: /^[A-Za-zА-Яа-яЁё0-9]+$/ })}
+            />
+            <div className="error-message">{errors.password && t('password_error')}</div>
+            <button type="submit" className="registration-btn button">
+              {t('sign_Up')}
+            </button>
+          </form>
+          <div>
+            <span>{t('registration_msg')}</span>
+            <NavLink to="/signin">
+              <span className="page-link">{t('to_autorization_page')}</span>
+            </NavLink>
+          </div>
         </div>
       </section>
     </>
