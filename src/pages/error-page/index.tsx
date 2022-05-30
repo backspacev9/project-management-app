a5fe2c37f5d50c3b5ffac9313ed3bc0dd468beb1
m.filionchyk <maria.filenchik@yandex.by>
2022-05-30 21:19:20 +0300

    feat: add style for error page

--- src/pages/error-page/index.tsx
+++ src/pages/error-page/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import Header from '../../components/header';
+import './index.scss';
 
 const ErrorPage = () => {
   const { t } = useTranslation();
   return (
     <>
-      <div>404</div>
-      <div>{t('page_404')}</div>
+      <section className="error-page">
+        <div>404</div>
+        <div>{t('page_404')}</div>
+      </section>
     </>
   );
 };
