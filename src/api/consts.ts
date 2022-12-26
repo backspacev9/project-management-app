af9b825417e357499d005e112c1a531f42fb5cc8
backspacev9 <backspacev9@gmail.com>
2022-12-26 23:31:53 +0300

    fix: change base url api

--- src/api/consts.ts
+++ src/api/consts.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = `https://project-management-app-2p1v.onrender.com/`;
+export const BASE_URL = `https://project-management-api-19z0.onrender.com/`;
 //export const BASE_URL = `https://final-pr-map.herokuapp.com/`;
